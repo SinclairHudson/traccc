c661a1af8bf28134f0c504600e8f9aec9dff0c93
SinclairHudson <hudson.sinclair00@gmail.com>
2023-06-09 13:11:05 -0400

    switched to openCV writing, solved the compression issue but may have created a speed ramping issue

--- draw.py
+++ draw.py
@@ -5,10 +5,8 @@ from torchvision.io import read_video, write_video
 from effects import *
 from tqdm import tqdm
 from filters import *
+import cv2
 
-def draw(video, tracks, effect):
-    for track in tqdm(tracks):
-        effect(video, track)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Draws effects on the video, based on the tracks")
@@ -18,10 +16,18 @@ if __name__ == "__main__":
     args = parser.parse_args()
     name = args.name
     effect = args.effect
+    # vid_generator = skvideo.io.vreader(f"io/{name}.mp4")
     vid_generator = skvideo.io.vreader(f"io/{name}.mp4")
     vid_writer = skvideo.io.FFmpegWriter(f"io/{name}_out.mp4")
     metadata = skvideo.io.ffprobe(f"io/{name}.mp4")
     frame_count = int(metadata['video']['@nb_frames'])
+    fps = metadata['video']['@r_frame_rate']
+    numerator, denominator = map(int, fps.split('/'))
+    fps = numerator / denominator
+    width = int(metadata['video']['@width'])
+    height = int(metadata['video']['@height'])  # we also probably need to rotate
+    fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+    opencv_out = cv2.VideoWriter(f'io/{name}_out.mp4', fourcc, fps, (height,width))
 
     with open(f"internal/{name}.yaml", 'r') as f:
         track_dictionary = yaml.safe_load(f)
@@ -37,6 +43,7 @@ if __name__ == "__main__":
     tracks = [track for track in tracks if standard_filter(track, min_age=int(args.min_age))]
 
     print("adding effect")
+    # for i, frame in tqdm(enumerate(vid_generator), total=frame_count):
     for i, frame in tqdm(enumerate(vid_generator), total=frame_count):
         relevant_tracks = [track for track in tracks if effect.relevant(track, i)]
         out_frame = frame
@@ -45,8 +52,9 @@ if __name__ == "__main__":
         for track in relevant_tracks:
             out_frame = effect.draw(out_frame, track, i)
 
-        vid_writer.writeFrame(out_frame)
+        bgr_frame = cv2.cvtColor(out_frame, cv2.COLOR_RGB2BGR)
+        opencv_out.write(bgr_frame)
 
-    vid_writer.close()
+    opencv_out.release()
     print(f"successfully wrote video io/{name}_out.mp4")
 
