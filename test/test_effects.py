099951cda06203680dfb2a0466c573cf3ce11dce
SinclairHudson <hudson.sinclair00@gmail.com>
2023-08-10 16:16:53 -0400

    adding test effects

---
+++ test/test_effects.py
@@ -0,0 +1,26 @@
+from effects import *
+import os
+import pytest
+
+def get_stormy():
+    if not os.path.exists("internal/stormy.npz"):
+        os.system("python3 detect.py stormy --input test_assets/stormy.mp4")
+        os.system("python3 track.py stormy")
+
+def get_stormy_vid_generator():
+    pass
+
+def test_fully_connected():
+    get_stormy()
+
+@pytest.mark.parametrize("effect_class", [FullyConnected,
+                                          FullyConnectedNeon,
+                                          Dot,
+                                          LaggingDot,
+                                          Line,
+                                          Debug,
+                                          HighlightLine,
+                                          Contrail])
+def test_effect(effect_class):
+    pass
+
